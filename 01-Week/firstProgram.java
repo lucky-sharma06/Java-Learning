59d761e655153d2660b19eea551a10b1c5fdb15b
Bhavya Sharma <mc.coding006@gmail.com>
2026-07-31 09:03:50 +0530

    Add files via upload

---
+++ 01-Week/firstProgram.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+
+class firstProgram{
+    public static void main(String args[]){
+        System.out.println("Hi");
+        int a = 0, b = 5;
+        System.out.println("The sum is " + (a + b));
+    }
+}
